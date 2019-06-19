e66541495c74aeea1ceb3aca1296dd9791af3bf5
luca <dongyanjie_neuq@163.com>
2019-06-19 16:56:35 +0800

    [ADD] install environment script

---
+++ installation_environment_script/ubuntu_gcc_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# install gcc5 on ubuntu 14.04
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install gcc-5 g++-5
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
